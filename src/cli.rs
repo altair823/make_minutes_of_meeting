ad7fbd1fba0560afb504bc869ab3f9ed7f720b1c
kthTP <dlsrks0734@gmail.com>
2024-04-30 22:55:19 +0900

    change filename logic

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, author, about, long_about)]
 pub struct Cli {
-    #[arg(default_value_t = String::from("momi_text"))]
     pub filename: String,
     #[arg(short, long, default_value_t = false)]
     pub overwrite: bool,
